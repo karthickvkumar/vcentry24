a5a919ec85bcfa6291fc9779c10d5871dd43c9a3
kvelraj_concord <kvelraj@concord.net>
2022-12-24 11:13:43 +0530

    JS Data types

---
+++ HTML/basics.js
@@ -0,0 +1,22 @@
+// Number -> Any numerical value from Positive, Negative, Fractional, Decimal
+
+// To create variable (Temp storage)
+// syntax 
+// var variable_name = value;
+
+// variable_name -> Any valid text, it should contains a-z, A-Z, 0-9, _, $
+
+var user_age = 28;
+console.log(user_age);
+
+// String -> Any text value which is typed inside '' or ""
+
+var user_emailid = "karthick@gmail.com";
+console.log(user_emailid);
+
+// Boolean -> Either true or false
+var result = true;
+console.log(result);
+
+var future_value;
+console.log(future_value);
