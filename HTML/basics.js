2faa6299a41b8a06c7c0d7ee12f12945c8864907
kvelraj_concord <kvelraj@concord.net>
2022-12-24 11:47:47 +0530

    js objects

--- HTML/basics.js
+++ HTML/basics.js
@@ -20,3 +20,35 @@ console.log(result);
 
 var future_value;
 console.log(future_value);
+
+user_emailid = "kumar@yahoo.in";
+console.log(user_emailid);
+
+var student_first_name = "karthick";
+var student_last_name = "kumar";
+var student_age = 28;
+var student_roll_number = 45785456;
+var student_standard = "XII";
+
+// Object -> It is a collection of Key and Values
+// Creating an Object
+// synatx 
+
+// var variable_name = {
+//   key1 : value,
+//   key2 : value,
+//   key3 : value
+// };
+
+// key -> Similar to variable_name -> it should contains a-z, A-Z, 0-9, _, $
+// value -> Number, String, Boolean, Object, Array, Function
+
+var student_information = {
+  first_name : "karthick",
+  last_name : "kumar",
+  age : 28,
+  roll_number : 457855,
+  standard : "XII"
+};
+
+console.log(student_information);
