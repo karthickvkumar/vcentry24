bfbb2df62664d787d4c32e2a700b71ec7b869aee
kvelraj_concord <kvelraj@concord.net>
2023-01-22 11:57:50 +0530

    soruce code

--- learning-react/src/components/header.js
+++ learning-react/src/components/header.js
@@ -1,8 +1,22 @@
 import React from "react";
 
 const HeaderComponent = () => {
+  let username = "karthick";
+  let address= {
+    street: "12, AMC Road",
+    area: "T.Nagar",
+    city: "Chennai"
+  };
+
+  const onLogout = () => {
+    alert("You've been successfully Logout!");
+  }
+
   return(
     <div>
+      <h2>Welcome to back, {username}</h2>
+      <button onClick={() => onLogout()}>Logout</button>
+      <h3>Location: {address.area}, {address.city}</h3>
       <h2 className="heading-text">Header of this page</h2>
     </div>
   )
