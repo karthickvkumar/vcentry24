d315f6422692c6ed91ec75fc18be2d91b710090c
kvelraj_concord <kvelraj@concord.net>
2023-01-29 10:35:05 +0530

    source code

--- learning-react/src/components/header.js
+++ learning-react/src/components/header.js
@@ -1,12 +1,18 @@
 import React from "react";
 
 const HeaderComponent = () => {
-  let username = "karthick";
-  let address= {
+  
+  const username = "karthick";
+  
+  const address = {
     street: "12, AMC Road",
     area: "T.Nagar",
     city: "Chennai"
   };
+
+  const list = [
+    "Mr.Abc", "Mr.Bcd", "Mr.Def"
+  ]
 
   const onLogout = () => {
     alert("You've been successfully Logout!");
@@ -14,9 +20,12 @@ const HeaderComponent = () => {
 
   return(
     <div>
-      <h2>Welcome to back, {username}</h2>
+      <h2>Welcome back, {username}</h2>
       <button onClick={() => onLogout()}>Logout</button>
       <h3>Location: {address.area}, {address.city}</h3>
+
+      <h3>{list[0]} , {list[2]}</h3>
+
       <h2 className="heading-text">Header of this page</h2>
     </div>
   )
