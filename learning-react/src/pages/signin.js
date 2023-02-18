446e853c8e555eb84c8ef0aeeb0999c13bc6152a
kvelraj_concord <kvelraj@concord.net>
2023-02-18 12:17:27 +0530

    source code

--- learning-react/src/pages/signin.js
+++ learning-react/src/pages/signin.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 import {useNavigate} from "react-router-dom";
 
 import HeaderComponent from "../components/header";
 
 const SignInPage = () => {
 
-  const navigate = useNavigate(); 
+  // const navigate = useNavigate(); 
+
+  // State Variable
+  // const [variableName, functionName] = useState(initialData); 
+  const [loginForm, loginFunction] = useState({ 
+    username : "",
+    password : ""
+  });
 
   const validateLogin = () => {
-    navigate("/contact");
+    // navigate("/contact");
+
+    //stateVaiable_variableName
+    console.log(loginForm);
+  }
+
+  const handleInputField = (event) => {
+    console.log(event.target.name, event.target.value);
+    
+    // stateVariable_FunctionName({...stateVaiable_variableName, [initialDataKey] : value});
+
+    loginFunction({...loginForm, [event.target.name] : event.target.value});
+
   }
 
   return(
@@ -18,11 +37,13 @@ const SignInPage = () => {
         <h1>Sign In</h1>
         <div className="space">
           <label className="label">Enter your Username</label>
-          <input className="input" type="text" placeholder="Enter your username" />
+          {/* name="initialDataKey" */}
+          <input className="input" type="text" placeholder="Enter your username" onChange={handleInputField} name="username" />
         </div>
         <div className="space">
           <label className="label">Enter your Password</label>
-          <input className="input" type="password" placeholder="Enter you password"/>
+          {/* name="initialDataKey" */}
+          <input className="input" type="password" placeholder="Enter you password" onChange={handleInputField} name="password"/>
         </div>
         <div>
           <button className="btn" onClick={() => validateLogin()}>Login</button>
