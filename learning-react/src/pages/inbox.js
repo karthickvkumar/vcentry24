e1fcdc47d367d7dd57eb78cad468c1737c697dc2
kvelraj_concord <kvelraj@concord.net>
2023-02-25 11:30:36 +0530

    source code

--- learning-react/src/pages/inbox.js
+++ learning-react/src/pages/inbox.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 
 const InboxPage = () => {
+
+  const [showHide, setDisplayMode] = useState(true);
+
+  const ShowImage = () => {
+    setDisplayMode(true);
+  }
+
+  const HideImage = () => {
+    setDisplayMode(false);
+  }
+
   return(
     <div>
       <h1>Here you can see all INBOX Mail</h1>
+      <div>
+        <button onClick={() => ShowImage() }>Show Image</button>
+        <button onClick={() => HideImage()}>Hide Image</button>
+      </div>
+      {
+        showHide &&
+        <img src={require("../images/shinchu.jpg")} width="300" alt="banner-image" />
+      }
     </div>
   )
 }
