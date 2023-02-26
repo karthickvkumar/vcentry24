3e53ba515fe6b2d53a46a49148874d81441fd809
kvelraj_concord <kvelraj@concord.net>
2023-02-26 11:57:26 +0530

    source code

--- learning-react/src/pages/home.js
+++ learning-react/src/pages/home.js
@@ -1,16 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 import HeaderComponent from "../components/header";
 import FooterComponent from "../components/footer";
-import axios from 'axios';
+import axios from "axios";
 
 const HomePage = () => {
+
+  const [userProfiles, UpdateUserList] = useState([]);
 
   const getUserProfile = () => {
     const url = "https://reqres.in/api/users?page=2";
 
     axios.get(url)
       .then( (response) => {
-          console.log(response.data);
+         const userList = response.data.data;
+         UpdateUserList(userList);
        })
       .catch( (error) => { 
         console.log(error);
@@ -24,7 +27,40 @@ const HomePage = () => {
       <div className="page-content">
         <h1>Welcome to Home Page</h1>
         <button onClick={() => getUserProfile()}>Load User Profile</button>
+
+        <table className="customers">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Email ID</th>
+              <th>Picture</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              userProfiles.map((value, index) => {
+                return(
+                  <tr>
+                    <td>{value.id}</td>
+                    <td>{value.first_name}</td>
+                    <td>{value.last_name}</td>
+                    <td>{value.email}</td>
+                    <td>
+                      <img src={value.avatar} className="profile-pic" />
+                    </td>
+                  </tr>
+                )
+              })
+            }
+          </tbody>
+        </table>
+      
+      
       </div>
+
+      
 
       <FooterComponent></FooterComponent>
     </div>
