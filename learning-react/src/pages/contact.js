7d4af97429db9c649cfe56bc7298e5fc7283a9da
kvelraj_concord <kvelraj@concord.net>
2023-03-11 12:42:53 +0530

    source code

--- learning-react/src/pages/contact.js
+++ learning-react/src/pages/contact.js
@@ -1,15 +1,31 @@
 import React from "react";
 import HeaderComponent from "../components/header";
 import FooterComponent from "../components/footer";
-
+import axios from "axios";
 
 const ContactPage = () => {
+
+  const loadUsers = () => {
+    const url = "http://localhost:4000/user-list";
+
+    axios.get(url)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+
+  }
+
   return(
     <div className="main-page">
       <HeaderComponent></HeaderComponent>
       
       <div className="page-content">
         <h1>Welcome to Contact Page</h1>
+
+        <button onClick={() => loadUsers()}>List Users</button>
       </div>
 
       <FooterComponent></FooterComponent>
--- node-api/app.js
+++ node-api/app.js
@@ -5,7 +5,27 @@ var mysql = require("mysql");
 var app = express();
 var http = require("http").createServer(app);
 
+app.use(cors({
+  credentials : true,
+  origin: "*"
+}));
+
+app.use(express.json());
+
+// http://localhost:4000/user-list
+app.get("/user-list", (request, response) => {
+  var users = [
+    {name: "Karthick", age: 28},
+    {name: "Kumar", age: 28},
+    {name: "John", age: 28},
+    {name: "Yuvi", age: 28},
+  ];
+
+  response.status(200).send(users);
+})
+
+
 var port = process.env.PORT || 4000;
 http.listen(port, () => {
   console.log("THE Node JS Sever is running on PORT 4000");
-})
+});
